5423cd0027bb095b6a0f06a84d838a249bbc8781
Wutzy <matmati.anis@gmail.com>
2023-08-23 03:04:40 +0200

    create skillBar component

---
+++ src/components/knowledges/skill.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+import React from 'react';
+
+const Skill: FunctionComponent = () => {
+    return (
+        <div>
+            Compétence composant
+        </div>
+    );
+};
+
+export default Skill;
