13115db35baa8ccfb865705edd2543b178c44222
Wutzy <matmati.anis@gmail.com>
2023-08-31 05:16:14 +0200

    add link of social logo to open them in new tab

--- src/components/knowledges/experiences/exp-item.tsx
+++ src/components/knowledges/experiences/exp-item.tsx
@@ -11,7 +11,7 @@ type ExperienceProps = {
 const ExperienceItem: FunctionComponent<ExperienceProps> = ({experience}) => {
     return (
         <div>
-            <p>{experience.name_corporation}: {experience.post_name}</p>                        
+            <p>{experience.name_corporation}: <span className='job-post'>{experience.post_name}</span></p>                        
             <span>De { formatDate(experience.date_entry) +' à ' +  formatDate(experience.date_release)}</span> 
             <span>({getDuration(experience.date_release, experience.date_entry)} mois)</span>        
         </div>
--- src/helpers/format-date.js
+++ src/helpers/format-date.js
@@ -1,6 +1,6 @@
 const formatDate = (date) => {
     const monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
-    return monthNames[date.getMonth()] + "-" + date.getFullYear();
+    return monthNames[date.getMonth()] + " " + date.getFullYear();
 }
 
 export default formatDate;
