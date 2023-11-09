449aea5ab367441fbc7458134ba9c3c3e42fea90
Wutzy <matmati.anis@gmail.com>
2023-11-09 16:07:24 +0100

    create model : SoftSkill

---
+++ src/models/mocked-data/mock-softs-skills.ts
@@ -0,0 +1,27 @@
+import SoftSkill from '../soft-skill';
+
+export const SOFTSKILLS: SoftSkill[] = [
+ {
+    id: 1,
+    name: "Analyse",
+ },
+ {
+    id: 2,
+    name: "Autonomie",
+ },
+ {
+    id: 3,
+    name: "Curiosité intellectuelle",
+ },
+ {
+    id: 4,
+    name: "Organisation",
+ },
+ {
+    id: 5,
+    name: "Travail d'équipe",
+ }
+
+];
+
+export default SOFTSKILLS;
